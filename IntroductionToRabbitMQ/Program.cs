5a8821410f33f329622c95635aba2414edb73614
milinda <milinda.seneviratne@chilindo.com>
2018-04-08 18:46:11 +0700

    Added all the basic code to connect to a RabbitMQ server.

--- IntroductionToRabbitMQ/Program.cs
+++ IntroductionToRabbitMQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,24 @@ namespace IntroductionToRabbitMQ
 {
     class Program
     {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Passowrd = "guest";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ  Queue Creator... ");
+            Console.WriteLine(Environment.NewLine);
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = UserName,
+                Password = Passowrd
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
         }
     }
 }
