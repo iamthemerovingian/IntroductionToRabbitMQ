1392ac2449a5735565d795f5c05d92d971e046e1
milinda <milinda.seneviratne@chilindo.com>
2018-04-08 21:27:39 +0700

    Added a OneWay Messaging client and sdcripts to build the queue.

---
+++ Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting RabbitMQ Message Sender....\n\n");
+
+            var messageCount = 0;
+            var sender = new RabbitSender();
+
+            Console.WriteLine("Press the enter key to send a message");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    var message = $"Message Count: {messageCount}";
+                    Console.WriteLine($"Sending: {message}");
+
+                    sender.Send(message);
+                    messageCount++;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Client/RabbitSender.cs
@@ -0,0 +1,77 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Client
+{
+    class RabbitSender
+    {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Password = "guest";
+        private const string QueueName = "Module2.Sample1";
+        private const string ExchangeName = "";
+        private const bool IsDurable = true;
+
+        private const string VirtualHost = "";
+        private int Port = 0;
+
+        private ConnectionFactory connectionFactory;
+        private IConnection connection;
+        private IModel model;
+
+        public RabbitSender()
+        {
+            DisplaySettings();
+            SetupRabbitMq();
+        }
+
+        private void DisplaySettings()
+        {
+            Console.WriteLine("Host: {0}", HostName);
+            Console.WriteLine("Username: {0}", UserName);
+            Console.WriteLine("Password: {0}", Password);
+            Console.WriteLine("QueueName: {0}", QueueName);
+            Console.WriteLine("ExchangeName: {0}", ExchangeName);
+            Console.WriteLine("VirtualHost: {0}", VirtualHost);
+            Console.WriteLine("Port: {0}", Port);
+            Console.WriteLine("Is Durable: {0}", IsDurable);
+        }
+
+        private void SetupRabbitMq()
+        {
+            connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = UserName,
+                Password = Password
+            };
+
+            if (!string.IsNullOrWhiteSpace(VirtualHost))
+            {
+                connectionFactory.VirtualHost = VirtualHost;
+            }
+
+            if (Port > 0)
+            {
+                connectionFactory.Port = Port;
+            }
+
+            connection = connectionFactory.CreateConnection();
+            model = connection.CreateModel();
+        }
+
+        internal void Send(string message)
+        {
+            //Create message properties..
+            IBasicProperties properties = model.CreateBasicProperties();
+            properties.Persistent = true;
+
+            //Create the message....
+            byte[] messageBuffer = Encoding.Default.GetBytes(message);
+
+            //Send Message...
+            model.BasicPublish(ExchangeName, QueueName, properties, messageBuffer);
+        }
+    }
+}
