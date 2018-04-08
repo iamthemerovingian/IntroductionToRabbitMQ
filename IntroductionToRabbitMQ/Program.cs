394a69569a6f862620654e1b7e4d4320ce79f89d
milinda <milinda.seneviratne@chilindo.com>
2018-04-08 20:08:02 +0700

    Added scripts and a bat file to run the scrips to automate the queue creation process.

--- IntroductionToRabbitMQ/Program.cs
+++ IntroductionToRabbitMQ/Program.cs
@@ -27,6 +27,22 @@ namespace IntroductionToRabbitMQ
 
             var connection = connectionFactory.CreateConnection();
             var model = connection.CreateModel();
+
+            //Creating a queue with name myqueue_codfe with durable messages but not exlusive, auto delete odd and no arguments.
+            model.QueueDeclare("myqueue_code", true, false, false, null);
+            Console.WriteLine("myqueue_code has been created...");
+
+            //Crete an exchange to connect to the queue. The type of the exhange is topic, not sure what that means yet...
+            model.ExchangeDeclare("myexchange_code", ExchangeType.Topic);
+            Console.WriteLine("myexchange_code has been created...");
+
+            //Binding the exchange to the queue, and adding the routing key "cars" so that only certian messages get route to this queue
+            model.QueueBind("myqueue_code", "myexchange_code", "cars");
+            Console.WriteLine("Exhane-Queue binding complete");
+
+            Console.ReadLine();
+
+
         }
     }
 }
