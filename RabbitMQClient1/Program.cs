8ba43578e7f1c67a22bbc37b4c001e3f9cf03755
milinda <milinda.seneviratne@chilindo.com>
2018-04-08 20:29:39 +0700

    Added a simple client that can send messages to specific queues using the default "" excchange.

---
+++ RabbitMQClient1/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQClient1
+{
+    class Program
+    {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Password = "guest";
+        private const string QueueName = "Module1.Sample3";
+        private const string ExchangeName = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Startig RabbitMQ Message Sender...\n\n");
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = UserName,
+                Password = Password
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            //Create message properties..
+            var properties = model.CreateBasicProperties();
+            properties.SetPersistent(false);
+
+            //Create the message....
+            byte[] message = Encoding.Default.GetBytes("Hello World");
+
+            //Send Message...
+            model.BasicPublish(ExchangeName, QueueName, properties, message);
+
+            Console.WriteLine("Message Sent...");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ RabbitMQClient1/SetupRabbitMQ.bat
@@ -0,0 +1,6 @@
+
+powershell.exe -noexit .\SetupRabbitMQ.ps1 -RabbitDllPath "C:\Users\iamth\source\repos\IntroductionToRabbitMQ\packages\RabbitMQ.Client.5.0.1\lib\net451\RabbitMQ.Client.dll"
+
+pause
+
+// C:\Users\iamth\source\repos\IntroductionToRabbitMQ\packages\RabbitMQ.Client.5.0.1\lib\net451\RabbitMQ.Client.dll
