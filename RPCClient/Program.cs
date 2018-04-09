b2f815a4ee520b54d221d8f6cc5cf4ae6d095446
Milinda.Seneviratne <milinda.seneviratne@chilindo.com>
2018-04-09 10:26:23 +0700

    Added a RPC Messaging example where one client sends a message and waits for a response.

---
+++ RPCClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting RabbitMQ Message Sender....\n\n");
+
+            var rpcClient = new RpcClientService();
+
+            Console.WriteLine("Press the enter key to send a message");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q)
+                {
+                    rpcClient.Close();
+                }
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine(" [x] Requesting fib(30)");
+                    var response = rpcClient.Call("30");
+                    Console.WriteLine(" [.] Got '{0}'", response);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
