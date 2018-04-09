9469a639618e30d92e568b4ad8c78e240cd866f2
Milinda.Seneviratne <milinda.seneviratne@chilindo.com>
2018-04-09 14:26:27 +0700

    Added a solution for fanout type messaging, where all connected servers get the message from the client.

---
+++ EmitLog/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmitLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            Console.WriteLine(" Press [enter] to send a message.");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    using (var connection = factory.CreateConnection())
+                    {
+                        using (var channel = connection.CreateModel())
+                        {
+                            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                            string message = GetMessage(args);
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish(exchange: "logs",
+                                                routingKey: "",
+                                                basicProperties: null,
+                                                body: body);
+
+                            Console.WriteLine($" [x] Sent {message}");
+                        }
+                    }
+                }
+            }
+            
+
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Length > 0 ? string.Join(" ", args) : "Hello World!!"; 
+        }
+    }
+}
---
+++ RecieveLogs/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecieveLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    channel.QueueBind(queue: queueName,
+                        exchange: "logs",
+                        routingKey: "");
+
+                    Console.WriteLine(" [*] waiting for logs. ");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += MessageRecieved;
+
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit. ");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void MessageRecieved(object sender, BasicDeliverEventArgs e)
+        {
+            byte[] body = e.Body;
+
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($" Message {message}");
+        }
+    }
+}
